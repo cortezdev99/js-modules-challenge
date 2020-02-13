117fceccd9ade96cc50377087782b7531ebf6362
Michale-Cortez <CortezDevelopement@gmail.com>
2020-02-12 21:45:31 -0700

    Initial Commit

---
+++ src/bootstrap.js
@@ -0,0 +1,7 @@
+import classChallenge, { greeting, sum} from './classChallenge'
+
+console.log(classChallenge())
+
+console.log(greeting)
+
+console.log(sum(2, 2))
---
+++ src/classChallenge.js
@@ -0,0 +1,9 @@
+export default function() {
+  return `<div>classChallenge</div>`
+};
+
+export const greeting = 'Welcome';
+
+export function sum(numOne, numTwo) {
+  return numOne + numTwo
+}
